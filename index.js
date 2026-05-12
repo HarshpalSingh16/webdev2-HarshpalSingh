4cc7a0abe63459eb3cc6e900f8126444d1eb7ab0
Harshpal Singh <harshpalsingh119@gmail.com>
2026-05-11 20:12:06 -0700

    assignment2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default;
 const bcrypt = require('bcrypt');
 const Joi = require('joi');
 const { MongoClient } = require('mongodb');
